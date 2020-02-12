d952625c28d232bcac88c281c8bd44fa61788557
Antoine de Maleprade <az0uz@users.noreply.github.com>
2020-02-12 13:37:26 -0800

    v0.1.1: added setup_requires with dependency link

--- setup.py
+++ setup.py
@@ -3,4 +3,19 @@ from setuptools import setup, find_packages
 install_requires=[
     "py-dummy-pkg @ git+ssh://git@github.com/az0uz/py-dummy-pkg.git@v0.1.0"
 ]
-setup(name="py-foo-pkg", version="0.1.0", python_requires=">=3.6", install_requires=install_requires, packages=find_packages())
+
+setup_requires=[
+    "py-dummy-pkg"
+]
+
+dependency_links=[
+    "git+ssh://git@github.com/az0uz/py-dummy-pkg.git@v0.1.0#egg=py-dummy-pkg-0.1.0"
+]
+
+setup(
+    name="py-foo-pkg",
+    version="0.1.1",
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    packages=find_packages())
